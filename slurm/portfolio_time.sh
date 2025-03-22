c1da3664fb7a8305ecaa3871b778e0428ccd4b1d
Irina <iywang@princeton.edu>
2025-03-22 01:50:29 -0400

    dfs

--- slurm/portfolio_time.sh
+++ slurm/portfolio_time.sh
@@ -4,11 +4,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=35
 #SBATCH --mem-per-cpu=3G
-#SBATCH --time=10:00:00
+#SBATCH --time=3:00:00
 #SBATCH -o /scratch/gpfs/iywang/mro_results/portfolio_output/portfolio_test_%A_.txt
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=iabirina@hotmail.com
-#SBATCH --array=3            # job array with index values 0, 1, 2, 3
+#SBATCH --array=0-3            # job array with index values 0, 1, 2, 3
 
 export MOSEKLM_LICENSE_FILE=/scratch/gpfs/iywang/mro_experiments/mosek/mosek.lic
 
@@ -16,6 +16,6 @@ module purge
 module load anaconda3/2024.2
 conda activate mroenv
 
-# python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 40 --T 3001 --fixed_time 2500 --interval 100 --Q 500 --K 15 --r_start 0
+python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 20 --T 3001 --fixed_time 2500 --interval 100 --Q 500 --K 15 --r_start 40
 
-python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 40 --T 10001 --fixed_time 7000  --interval 100 --Q 500 --K 5 --r_start 0
+# python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 40 --T 10001 --fixed_time 7000  --interval 100 --Q 500 --K 5 --r_start 0
