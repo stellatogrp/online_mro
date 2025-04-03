227a56adec5982e6ea0e03e729e510838093fc43
Irina <iywang@princeton.edu>
2025-04-03 14:01:59 -0400

    new exps

--- slurm/portfolio_time.sh
+++ slurm/portfolio_time.sh
@@ -16,6 +16,6 @@ module purge
 module load anaconda3/2024.2
 conda activate mroenv
 
-python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 20 --T 1001 --fixed_time 800 --interval 50 --Q 300 --K 15 --N_init 5 --r_start 40
+# python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 20 --T 1001 --fixed_time 800 --interval 50 --Q 300 --K 15 --N_init 5 --r_start 40
 
-# python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 20 --T 10001 --fixed_time 7000  --interval 100 --Q 500 --K 5 --r_start 40 
+python portfolio_time/portMIP.py --foldername /scratch/gpfs/iywang/mro_results/portfolio/ --R 20 --T 10001 --fixed_time 7000  --interval 100 --Q 500 --K 5 --r_start 40
