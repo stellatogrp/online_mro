f6bd6e4608b2fdf27cea8825eceb484ce7f430d3
Irina <iywang@princeton.edu>
2026-05-26 17:04:54 -0400

    update interval default

--- port_new/port_DRO_orig.py
+++ port_new/port_DRO_orig.py
@@ -812,8 +812,8 @@ if __name__ == '__main__':
     parser.add_argument('--T', type=int, default=3001)
     parser.add_argument('--R', type=int, default=5)
     parser.add_argument('--m', type=int, default=30)
-    parser.add_argument('--interval', type=int, default=100)
-    parser.add_argument('--interval_SAA', type=int, default=100)
+    parser.add_argument('--interval', type=int, default=1)
+    parser.add_argument('--interval_SAA', type=int, default=1)
 
     parser.add_argument('--N_init', type=int, default=50)
     parser.add_argument('--r_start', type=int, default=0)
