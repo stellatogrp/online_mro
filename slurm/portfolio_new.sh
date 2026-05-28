170c68edc1518a084ce89ff5722a4a2d12e4b271
Irina <iywang@princeton.edu>
2026-05-28 13:28:16 -0400

    dpp

--- slurm/portfolio_new.sh
+++ slurm/portfolio_new.sh
@@ -8,7 +8,6 @@
 #SBATCH -o /scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/portfolio_test_p2_%A_.txt
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=iabirina@hotmail.com
-#SBATCH --array=0-2          # job array with index values 0, 1, 2, 3
 
 cd "$SLURM_SUBMIT_DIR"
 
@@ -18,13 +17,13 @@ module purge
 module load anaconda3/2024.2
 conda activate lropt_rev
 
-# python port_new/port_orig.py --foldername port_new/results/p1/5/ --R 10 --T 2001 --fixed_time 2001 --interval 1 --Q 500 --K 15 --N_init 5 --r_start 0 --m 50
+# python port_new/port_orig.py --foldername port_new/results/orig/p1/1/ --R 5 --T 2001 --fixed_time 2001 --interval 10 --Q 500 --K 15 --N_init 5 --r_start 5 --m 150
 
-python port_new/port.py --foldername port_new/results/p1/5/ --R 10 --T 2001 --fixed_time 2001 --interval 1 --Q 500 --K 15 --N_init 5 --r_start 0 --m 50
+# python port_new/port.py --foldername port_new/results/new/p1/1/ --R 10 --T 2001 --fixed_time 2001 --interval 1 --Q 500 --K 15 --N_init 5 --r_start 0 --m 150
 
-# python port_new/port_DRO_p2.py --foldername port_new/results/p2/5/ --R 5 --T 2001 --interval 1 --N_init 5 --r_start 0 --m 50
+python port_new/port_DRO.py --foldername port_new/results/new/p1/1/ --R 2 --T 2001 --interval 1 --N_init 5 --r_start 0 --m 150
 
-# python port_new/port_DRO_orig_p2.py --foldername port_new/results/p2/5/ --R 5 --T 2001 --interval 1 --N_init 5 --r_start 5 --m 50
+# python port_new/port_DRO_orig_p2.py --foldername port_new/results/orig/p2/1/ --R 5 --T 2001 --interval 10 --interval_SAA 10 --N_init 5 --r_start 5 --m 150
 
 
 # python portfolio_new/port.py --foldername portfolio_new/results/ --R 10 --T 10000 --fixed_time 8500  --interval 500 --Q 500 --K 5 --r_start 20 --m 50 --N_init 5
