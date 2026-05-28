aa2ccb0f1309ea09b4ae612396f1f1aca880b0e1
Irina <iywang@princeton.edu>
2026-05-27 23:04:11 -0400

    dro exps

--- slurm/portfolio_new.sh
+++ slurm/portfolio_new.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=35
 #SBATCH --mem-per-cpu=2G
-#SBATCH --time=20:00:00
+#SBATCH --time=7:00:00
 #SBATCH -o /scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/portfolio_test_p2_%A_.txt
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=iabirina@hotmail.com
@@ -22,10 +22,7 @@ python port_new/portnew.py --foldername port_new/results/p1/3/ --R 10 --T 2001 -
 
 # python port_new/port_p2.py --foldername port_new/results/p2/2/ --R 10 --T 2001 --fixed_time 2001 --interval 1 --Q 500 --K 15 --N_init 5 --r_start 0 --m 50
 
-
-
-
-# python port_new/port_DRO.py --foldername port_new/results/p1/2/ --R 10 --T 2001 --interval 1 --N_init 5 --r_start 0 --m 50
+# python port_new/port_DRO_p2.py --foldername port_new/results/p2/2/ --R 5 --T 2001 --interval 1 --N_init 5 --r_start 5 --m 50
 
 # python port_new/port_DRO_orig_p2.py --foldername port_new/results/p2/2/ --R 10 --T 2001 --interval 1 --N_init 5 --r_start 0 --m 50
 
