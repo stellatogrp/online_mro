9203439795f7db0cc042efc612e835180306d2a0
Irina <iywang@princeton.edu>
2026-05-31 15:58:22 -0400

    pltos

--- port_new/plots.py
+++ port_new/plots.py
@@ -832,31 +832,31 @@ def infer_end_ind(df_dict, K=None, t_col='t'):
     return int(len(dfk))
 
 
-foldername_orig = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/orig/p1/3/T'+str(T-1)+'R'+str(R)+'/'
+foldername_orig = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/orig/p1/5/T'+str(T-1)+'R'+str(R)+'/'
 
-folderout_orig = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/orig/3/T'+str(T-1)+'R'+str(R)+'/'
-
-os.makedirs(folderout_orig, exist_ok=True)
-
-foldername_orig_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/orig/p1/3/T'+str(T-1)+'R'+str(R)+'/'
-
-folderout_orig_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/orig/3/T'+str(T-1)+'R'+str(R)+'/'
+folderout_orig = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/orig/5/T'+str(T-1)+'R'+str(R)+'/'
 
 os.makedirs(folderout_orig, exist_ok=True)
 
-foldername_new = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/new/p1/3/T'+str(T-1)+'R'+str(R)+'/'
+foldername_orig_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/orig/p1/5/T'+str(T-1)+'R'+str(R)+'/'
 
-folderout_new = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/new/3/T'+str(T-1)+'R'+str(R)+'/'
+folderout_orig_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/orig/5/T'+str(T-1)+'R'+str(R)+'/'
+
+os.makedirs(folderout_orig, exist_ok=True)
+
+foldername_new = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/new/p1/5/T'+str(T-1)+'R'+str(R)+'/'
+
+folderout_new = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/new/5/T'+str(T-1)+'R'+str(R)+'/'
 
 os.makedirs(folderout_new, exist_ok=True)
 
-foldername_new_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/new/p1/7/T'+str(T-1)+'R'+str(R)+'/'
+foldername_new_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/results/new/p1/8/T'+str(T-1)+'R'+str(R)+'/'
 
-folderout_new_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/new/7/T'+str(T-1)+'R'+str(R)+'/'
+folderout_new_dro = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/new/8/T'+str(T-1)+'R'+str(R)+'/'
 
 os.makedirs(folderout_new, exist_ok=True)
 
-folderout = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/comp/3/T'+str(T-1)+'R'+str(R)+'/'
+folderout = '/scratch/gpfs/BSTELLATO/iywang/low_rank/online_mro/port_new/plots_new/comp/5/T'+str(T-1)+'R'+str(R)+'/'
 
 os.makedirs(folderout, exist_ok=True)
 
@@ -883,13 +883,13 @@ end_ind_new = infer_end_ind(df_new, K = 25)
 plot_eval_all_compare(
     df_orig, quantiles_orig, df_orig_dro, quantiles_orig_dro,
     df_new, quantiles_new, df_new_dro, quantiles_new_dro,
-    j=(0,0,0), j_grad=(0,0,0), K=25, q=(25,75),
+    j=(0,2,0), j_grad=(0,2,0), K=25, q=(25,75),
     end_ind=end_ind_orig, end_ind_grad= end_ind_new,val2=2.3, legend=True,
 )
 
 plot_eval_compare(
    df_orig, quantiles_orig, df_orig_dro, quantiles_orig_dro,
     df_new, quantiles_new, df_new_dro, quantiles_new_dro,
-    j=(0,0,0), j_grad=(0,0,0), K=25, q=(25,75),
+    j=(0,2,0), j_grad=(0,2,0), K=25, q=(25,75),
     end_ind=end_ind_orig, end_ind_grad= end_ind_new, legend=True,
 )
